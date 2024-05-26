e7391bb546fcbf6c3330bc0e9eb2c040dfbb78ae
hyukjin0419 <21900771@handong.ac.kr>
2024-05-26 23:25:20 +0900

    create the design of the card

--- lib/home.dart
+++ lib/home.dart
@@ -14,8 +14,38 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: const Text('Hello world'),
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+      ),
+      body: const Center(
+        child: FolderCard(),
+      ),
+    );
+  }
+}
+
+class FolderCard extends StatelessWidget {
+  const FolderCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // MediaQuery를 사용하여 화면 크기 측정
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    // 가로 크기를 화면의 80%로 설정
+    double cardWidth = screenWidth * 0.8;
+    // 세로 크기를 가로 크기의 1/3.5로 설정
+    double cardHeight = cardWidth / 3.5;
+
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      elevation: 5,
+      color: const Color.fromRGBO(245, 245, 245, 1.0),
+      child: SizedBox(
+        width: cardWidth,
+        height: cardHeight,
+      ),
     );
   }
 }
