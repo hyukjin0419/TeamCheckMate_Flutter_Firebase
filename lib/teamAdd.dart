a5fd6c7bf90aa6514fcbe2565aca45589aaab7a1
hyukjin0419 <21900771@handong.ac.kr>
2024-05-28 15:04:41 +0900

    team page front is ready

--- lib/teamAdd.dart
+++ lib/teamAdd.dart
@@ -14,6 +14,8 @@ class TeamAddPage extends StatefulWidget {
 }
 
 class _TeamAddPageState extends State<TeamAddPage> {
+  final _titlecontroller = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     var appState = Provider.of<ApplicationState>(context, listen: true);
@@ -41,18 +43,32 @@ class _TeamAddPageState extends State<TeamAddPage> {
         ),
         actions: <Widget>[
           TextButton(
-            onPressed: () {},
             child: const Text('확인',
                 style: TextStyle(color: Colors.black, fontSize: 18)),
+            onPressed: () {
+              if (_formKey.currentState?.validate() ?? false) {
+                context.go('/home');
+              }
+            },
           ),
         ],
       ),
-      body: const Padding(
-        padding: EdgeInsets.all(8.0),
-        child: TextField(
-          decoration: InputDecoration(
-            border: OutlineInputBorder(),
-            hintText: '팀 이름을 입력하세요',
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _formKey,
+          child: TextFormField(
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return '팀 이름을 입력하세요';
+              }
+              return null;
+            },
+            controller: _titlecontroller,
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              hintText: '팀 이름을 입력하세요',
+            ),
           ),
         ),
       ),
